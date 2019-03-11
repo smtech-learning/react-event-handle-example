9b3782c7cb824e419016e8367b2d844d099d0ca3
agtechworld <sureshmunnangi@gmail.com>
2019-03-10 22:48:50 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    todos : ['chek weather', 'get cofee']
+  }
+
+   callEvent = () => {
+    console.log('Called the event');
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <button onClick={this.callEvent}>  Save </button>
       </div>
     );
   }
